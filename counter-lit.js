48780d062cd3551bf9f82b73f7e886f546b5e70f
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-06-21 18:31:01 -0500

    progress

---
+++ counter-lit.js
@@ -0,0 +1,62 @@
+import {
+  css,
+  html,
+  LitElement,
+} from "https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js";
+
+class CounterLit extends LitElement {
+  static properties = {
+    count: { type: Number },
+  };
+
+  static styles = css`
+    button {
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+
+      aspect-ratio: 1;
+      border-radius: 50%;
+      color: white;
+      font-size: inherit;
+      height: 2.5rem;
+    }
+
+    #decrement-btn {
+      background-color: red;
+    }
+
+    #increment-btn {
+      background-color: green;
+    }
+
+    div {
+      font-size: 2rem;
+    }
+  `;
+
+  constructor() {
+    super();
+    this.count = 0;
+  }
+
+  decrement() {
+    if (this.count > 0) this.count--;
+  }
+
+  increment() {
+    this.count++;
+  }
+
+  render() {
+    return html`
+      <div>
+        <button id="decrement-btn" @click=${this.decrement}>-</button>
+        <span>${this.count}</span>
+        <button id="increment-btn" @click=${this.increment}>+</button>
+      </div>
+    `;
+  }
+}
+
+customElements.define("counter-lit", CounterLit);
